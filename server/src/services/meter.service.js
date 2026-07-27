af320bd054e1bd2432ea06f332eaef0f9d5fba49
tunaparlak0 <tuna.parlak@ogr.sakarya.edu.tr>
2026-07-27 16:07:13 +0300

    code cleaning v2

--- server/src/services/meter.service.js
+++ server/src/services/meter.service.js
@@ -4,17 +4,16 @@ const { formatMeterNo, generateSubscriptionId } = require('../utils/idGenerator'
 const createMeter = async (data) => {
   const { address } = data;
 
-  // 1. Sayaç numarasını güvenli şekilde hesapla
   const meterCount = await prisma.meter.count();
   const nextNumber = meterCount + 1;
   const newMeterNo = formatMeterNo(nextNumber); // Örn: "000001"
 
-  // 2. Abonelik ID'sini güvenli şekilde hesapla
+  
   const subCount = await prisma.subscription.count();
   const nextSubNumber = 1000001 + subCount;
   const newSubId = generateSubscriptionId(nextSubNumber); // Örn: "1000001"
 
-  // 3. Önce Meter'ı oluştur (içinde subscriptions bloğu YOK)
+  
   const newMeter = await prisma.meter.create({
     data: {
       meterNo: newMeterNo,
@@ -22,7 +21,7 @@ const createMeter = async (data) => {
     }
   });
 
-  // 4. Ardından aboneliği bu sayaç numarasına bağlı olarak oluştur
+  
   const newSubscription = await prisma.subscription.create({
     data: {
       id: newSubId,
